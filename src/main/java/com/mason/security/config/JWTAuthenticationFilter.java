8a10fb2a7306586b60eeb1dcabc07284a35f91c6
DESKTOP-NELQMJ0\Mason <willy55406@gmail.com>
2024-10-23 14:45:31 +0800

    註解新增與debug

--- src/main/java/com/mason/security/config/JWTAuthenticationFilter.java
+++ src/main/java/com/mason/security/config/JWTAuthenticationFilter.java
@@ -43,20 +43,26 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter{
         jwt = authHeader.substring(7);
         //  先做好service在進入這一步  extract userEmail from JWT token 解析token拿出username(Email)
         userEmail = JwtService.extractUsername(jwt);
+        //SecurityContextHolder可以判斷使用者在Spring security是否已經被驗證過了
         if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
             if(JwtService.isTokenValid(jwt, userDetails)){
+                //建立一個新的 UsernamePasswordAuthenticationToken 物件，以更新 Spring Security 中的認證資訊。
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                     userDetails, 
                     null,
                     userDetails.getAuthorities()
                 );
+                // 為 authToken 附加來自當前 HTTP 請求的詳細資訊（如 IP 地址和會話 ID）。
                 authToken.setDetails(
+                    //WebAuthenticationDetailsSource().buildDetails(request) 的作用是根據當前的 HTTP 請求（request）生成一個 WebAuthenticationDetails 對象，這個對象包含了與當前會話有關的更多資訊
                     new WebAuthenticationDetailsSource().buildDetails(request)
                 );
+                // 將經過驗證的 authToken 放入當前的 SecurityContext 中，表示用戶已成功認證。
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
+        //將當前請求交給下一個過濾器
         filterChain.doFilter(request,response);
     }
     
--- src/main/java/com/mason/security/config/SecurityConfiguration.java
+++ src/main/java/com/mason/security/config/SecurityConfiguration.java
@@ -2,34 +2,45 @@ package com.mason.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
-@EnableWebFluxSecurity
-@NoArgsConstructor
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 
 
+    private final  JWTAuthenticationFilter JwTAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests()
-            .requestMatchers(null)
-            .permitAll()
-            .anyRequest()
-            .authenticated()
+            //將csrf保護機制禁用 因為已經使用JWT驗證了 不需要csrf機制
+            .csrf()
+            .disable()
+            .authorizeHttpRequests()//開始設置授權請求
+            .requestMatchers("")//指定公開訪問的路徑
+            .permitAll()//任何人都可以訪問
+            .anyRequest()//其他所有請求
+            .authenticated()//都要進行身分驗證
             .and()
-            .sessionMangement()
+            .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .authenticationProvider(authenticationProvider)
-            .addFilterBefore(JWTAuthfi)
+            .addFilterBefore(JwTAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+
+            
 
         
         return http.build();
